237949f37b6a7e42816ddce463d663ee2619a1ef
mroshaw <oli@mroshaw.co.uk>
2021-05-23 14:55:05 +0100

    Initial commit

---
+++ PrawnRepairAndCharge_BZ/PrawnRepairAndCharge_BZ/PrawnMod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HarmonyLib;
+using SMLHelper.V2;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger = QModManager.Utility.Logger;
+
+namespace PrawnRepairAndCharge_BZ
+{
+    class PrawnMod
+    {
+        [HarmonyPatch(typeof(Exosuit))]
+        [HarmonyPatch("OnDockedChanged")]
+        internal class ExoSuitDock
+        {
+            [HarmonyPostfix]
+            public static void Prefix(Exosuit __instance, bool docked, Vehicle.DockType dockType)
+            {
+                Logger.Log(Logger.Level.Info, $"In pre fix {docked}");
+            }
+        }
+    }
+}
---
+++ SeaTruckSpeedModifier_BZ/SeaTruckSpeedMod_BZ/SeaTruckMod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HarmonyLib;
+using SMLHelper.V2;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger = QModManager.Utility.Logger;
+
+namespace SeaTruckSpeedMod_BZ
+{
+    class SeaTruckMod
+    {
+        [HarmonyPatch(typeof(SeaTruckMotor))]
+        [HarmonyPatch("GetWeight")]
+        internal class SeaTruckWeight
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ref float __result)
+            {
+                Logger.Log(Logger.Level.Info, $"In post fix with weight {__result}");
+                float modifier = SeaTruckSpeedQMod.Config.SeaTruckSpeedModifier;
+                Logger.Log(Logger.Level.Info, $"Modifying with {modifier}");
+                float newWeight = __result / modifier;
+                Logger.Log(Logger.Level.Info, $"New weight {newWeight}");
+                __result = newWeight;
+            }
+        }
+    }
+}
