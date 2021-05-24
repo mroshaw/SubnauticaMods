9f34d4f4e8a7f9b40dff0ae198de67188c7ef77d
mroshaw <oli@mroshaw.co.uk>
2021-05-24 19:54:58 +0100

    First commit - framework done

---
+++ VehicleSpeedModifier_BZ/VehicleSpeedMod_BZ/PrawnSuitHistoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleSpeedMod_BZ
+{
+    /// <summary>
+    /// This class allows us to keep track of Prawn Suits that we've modded so we can dynamically
+    /// change the modifier in real time.
+    /// </summary>
+    /// 
+    internal class PrawnSuitHistoryItem
+    {
+        public Exosuit VehicleInstance;
+        public float VehicleValue;
+
+        public PrawnSuitHistoryItem(Exosuit vehicleInstance, float vehicleValue)
+        {
+            VehicleInstance = vehicleInstance;
+            VehicleValue = vehicleValue;
+        }
+    }
+}
---
+++ VehicleSpeedModifier_BZ/VehicleSpeedMod_BZ/SnowFoxMod.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+using System.Collections.Generic;
+
+namespace VehicleSpeedMod_BZ
+{
+    class SnowFoxMod
+    {
+        /// <summary>
+        /// Harmony hooks to modify the SeaTruck drag coefficient, reducing it to increase maximum speed
+        /// </summary>
+        /// 
+        [HarmonyPatch(typeof(Hoverbike))]
+        [HarmonyPatch("Start")]
+        internal class SnowFoxSpeed
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Hoverbike __instance)
+            {
+                // Grab the modifier value from Config and apply to the drag coefficient
+                Logger.Log(Logger.Level.Debug, $"In post fix");
+
+                // Get current value
+                float currentValue = 0.0F;
+                // Add to our list of instances to allow ad-hoc change to the value
+                SnowFoxHistoryItem newSnowFox = new SnowFoxHistoryItem(__instance, currentValue);
+                QMod.SnowFoxHistory.Add(newSnowFox);
+
+                // Get current modifier
+                float modifier = QMod.Config.SnowFoxSpeedModifier;
+                Logger.Log(Logger.Level.Debug, $"Modifier: {modifier}");
+
+                // Apply modifier
+                float newValue = 0.0F;
+                Logger.Log(Logger.Level.Debug, $"New value: {newValue}");
+
+            }
+        }
+    }
+}
