69a288a321ac1ee14a210d900f06a8c5e316c48a
mroshaw <oli@mroshaw.co.uk>
2021-06-11 15:55:52 +0100

    Initial commit
    Initial commit

---
+++ MyFirstSubnauticaMod_BZ/MyFirstMod.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace MyFirstSubnauticaMod_BZ
+{
+    /// <summary>
+    /// Class to mod the knife
+    /// </summary>
+    class MyFirstMod
+    {
+        [HarmonyPatch(typeof(Knife))]
+        [HarmonyPatch("Start")]
+        internal class KnifeDamageMod
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Knife __instance)
+            {
+                // Double the knife damage
+                float knifeDamage = __instance.damage;
+                float newKnifeDamage = knifeDamage * 2;
+                __instance.damage = newKnifeDamage;
+                Logger.Log(Logger.Level.Debug, $"Knife damage was: {knifeDamage}," +
+                    $" is now: {newKnifeDamage}");
+            }
+        }
+    }
+}
---
+++ MyFirstSubnauticaMod_BZ/QMod.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+
+
+namespace MyFirstSubnauticaMod_BZ
+{
+    [QModCore]
+    public static class QMod
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var modName = ($"<someuniquevalue>_{assembly.GetName().Name}");
+            Logger.Log(Logger.Level.Info, $"Patching {modName}");
+            Harmony harmony = new Harmony(modName);
+            harmony.PatchAll(assembly);
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+
+        }
+
+    }
+}
