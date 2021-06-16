bb8d0c2a4aa089126c42b5244b8b02cd9075786c
mroshaw <oli@mroshaw.co.uk>
2021-06-16 22:45:11 +0100

    Initial framework created

---
+++ SeaTruckAquariumEnhanced_BZ/Qmod.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Options.Attributes;
+using UnityEngine.UI;
+using Logger = QModManager.Utility.Logger;
+
+namespace SeaTruckAquariumEnhanced_BZ
+{
+    [QModCore]
+    public static class QMod
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            string id = "mroshaw_" + executingAssembly.GetName().Name;
+            Logger.Log(Logger.Level.Info, "Patching " + id);
+            new Harmony(id).PatchAll(executingAssembly);
+            Logger.Log(Logger.Level.Info, "Patched successfully!");
+        }
+    }
+}
