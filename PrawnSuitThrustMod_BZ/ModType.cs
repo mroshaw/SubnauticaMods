f30a9cd0089767644094c46dd36ef262e549bba4
mroshaw <oli@mroshaw.co.uk>
2021-06-16 22:33:46 +0100

    Initial release

---
+++ PrawnSuitThrustMod_BZ/ModType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrawnSuitThrustMod_BZ
+{
+    // Use these to track the type of mod being applied
+    internal enum ModType
+    {
+        DockAndCharge,
+        PowerDrill,
+        SuperThrust,
+        SuperSpeed,
+        Invincibility,
+    }
+}
