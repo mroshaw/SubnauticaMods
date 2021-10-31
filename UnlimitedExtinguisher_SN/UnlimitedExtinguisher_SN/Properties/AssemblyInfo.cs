71853534c8a0ce0a7a3b92caec2ac4316564676d
mroshaw <oli@mroshaw.co.uk>
2021-10-31 13:11:40 +0000

    Renamed to "SN" for consistency

--- UnlimitedExtinguisher_SN/UnlimitedExtinguisher_SN/Properties/AssemblyInfo.cs
+++ UnlimitedExtinguisher_SN/UnlimitedExtinguisher_SN/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnlimitedExtinguisher")]
+[assembly: AssemblyTitle("UnlimitedExtinguisher_SN")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnlimitedExtinguisher")]
+[assembly: AssemblyProduct("UnlimitedExtinguisher_SN")]
 [assembly: AssemblyCopyright("Copyright ©Iain Ollerenshaw  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
