34603db4ab2fb7320bf09f1fa84746e4d073ec87
mroshaw <oli@mroshaw.co.uk>
2023-01-09 17:22:28 +0000

    Updated to remove QMM dependency, now only requires BepInEx

---
+++ PrawnRepairAndCharge_BZ/PrawnRepairAndCharge_BZ/PrawnSuitRepairAndChargeMod_BZ.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+
+namespace Mroshaw.PrawnSuitRepairAndCharge_BZ
+{
+    class PrawnSuitRepairAndChargeMod_BZ
+    {
+        [HarmonyPatch(typeof(Exosuit))]
+        internal class Exosuit_Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(nameof(Exosuit.OnDockedChanged))]
+            public static void OnDockedChanged_Prefix(Exosuit __instance, bool docked, Vehicle.DockType dockType)
+            {
+                PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug("In Exosuit.OnDockedChanged");
+                if (docked)
+                {
+                    PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug($"Dock Change at: {dockType}, Docked is: {docked}");
+
+                    // If we're docked in MoonPool or SeaTruck, and option is enabled, make the modifications
+                    if ((dockType == Vehicle.DockType.Base && PrawnSuitRepairAndChargePlugin_BZ.EnableInMoonPool.Value) ||
+                        (dockType == Vehicle.DockType.Seatruck && PrawnSuitRepairAndChargePlugin_BZ.EnableInSeaTruck.Value))
+                    {
+                        Vehicle baseInstance = (Vehicle)__instance;
+                        // Get current and max health
+                        float currentHealth = __instance.liveMixin.health;
+                        float maxHealth = __instance.liveMixin.maxHealth;
+                        PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug($"Current health: {currentHealth}, Max health: {maxHealth}");
+                        float deltaHealth = maxHealth - currentHealth;
+
+                        // Top up health
+                        __instance.liveMixin.AddHealth(deltaHealth);
+                        PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug($"Added health delta: {deltaHealth}");
+
+                        // Get current charge and max charge
+                        baseInstance.energyInterface.GetValues(out float currentCharge, out float currentCapacity);
+                        PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug($"Current charge: {currentCharge}, Max charge: {currentCapacity}");
+                        float deltaPower = currentCapacity - currentCharge;
+
+                        // Top up charge
+                        baseInstance.AddEnergy(deltaPower);
+                        PrawnSuitRepairAndChargePlugin_BZ.Log.LogDebug($"Added power delta: {deltaPower}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PrawnRepairAndCharge_BZ/PrawnRepairAndCharge_BZ/PrawnSuitRepairAndChargePlugin_BZ.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Mroshaw.PrawnSuitRepairAndCharge_BZ
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class PrawnSuitRepairAndChargePlugin_BZ : BaseUnityPlugin
+    {
+        // Plugin properties
+        private const string myGUID = "com.mroshaw.prawnsuitrepairandchargemodbz";
+        private const string pluginName = "Prawn Suit Repair And Charge Mod BZ";
+        private const string versionString = "2.0.0";
+
+        // Config properties
+        private const string enableInMoonpoolConfigKey = "Enable in Moonpool";
+        private const string enableInSeaTruckConfigKey = "Enable on SeaTruck";
+
+        // Static config settings
+        public static ConfigEntry<bool> EnableInMoonPool;
+        public static ConfigEntry<bool> EnableInSeaTruck;
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            // Modifier config
+            EnableInMoonPool = Config.Bind("General",
+                enableInMoonpoolConfigKey,
+                true,
+                "Enabled in Moonpool docking");
+
+            EnableInSeaTruck = Config.Bind("General",
+                enableInSeaTruckConfigKey,
+                true,
+                "Enabled on Seatruck docking");
+
+            // Patch in our MOD
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            Log = Logger;
+        }
+    }
+}
+
