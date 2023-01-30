891f2fd763911cedc10801711cb614335a30473f
mroshaw <oli@mroshaw.co.uk>
2023-01-30 12:00:50 +0000

    Refactored

--- SeaTruckFishScoop_BZ/Properties/AssemblyInfo.cs
+++ SeaTruckFishScoop_BZ/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SeaTruckFishScoop_BZ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Daft Apple Games")]
 [assembly: AssemblyProduct("SeaTruckFishScoop_BZ")]
-[assembly: AssemblyCopyright("Copyright ©  2021 / 2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
