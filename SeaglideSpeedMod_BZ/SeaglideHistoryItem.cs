1f35e704682f8240f434820176782a49b9f20173
mroshaw <oli@mroshaw.co.uk>
2023-02-07 17:26:34 +0000

    New mod on request

---
+++ SeaglideSpeedMod_BZ/SeaglideHistoryItem.cs
@@ -0,0 +1,33 @@
+﻿namespace DaftAppleGames.SeaglideSpeedMod_BZ
+{
+    /// <summary>
+    /// Class to allow us to maintain a list of patches Seaglides
+    /// </summary>
+    internal class SeaglideHistoryItem
+    {
+        // Public properties
+        public Seaglide SeaglideInstance;
+        public float SeaglideForce;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="seaglideInstance"></param>
+        /// <param name="seaglideForce"></param>
+        public SeaglideHistoryItem(Seaglide seaglideInstance, float seaglideForce)
+        {
+            SeaglideInstance = seaglideInstance;
+            SeaglideForce = seaglideForce;
+        }
+
+        /// <summary>
+        /// Constructor with implicit force from instance
+        /// </summary>
+        /// <param name="seaglideInstance"></param>
+        public SeaglideHistoryItem(Seaglide seaglideInstance)
+        {
+            SeaglideInstance = seaglideInstance;
+            SeaglideForce = seaglideInstance.powerGlideForce;
+        }
+    }
+}
---
+++ SeaglideSpeedMod_BZ/SeaglideSpeedModPatches.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+
+namespace DaftAppleGames.SeaglideSpeedMod_BZ
+{
+    /// <summary>
+    /// Class to patch in game classes and methods
+    /// </summary>
+    internal class SeaglideSpeedModPatches
+    {
+        /// <summary>
+        /// Seaglide patches
+        /// </summary>
+        [HarmonyPatch(typeof(Seaglide))]
+        internal class SeaglidePatches
+        {
+            /// <summary>
+            /// Patch the Seaglide properties on start
+            /// </summary>
+            /// <param name="__instance"></param>
+            [HarmonyPatch(nameof(Seaglide.Start))]
+            [HarmonyPostfix]
+            public static void Start_Postfix(Seaglide __instance)
+            {
+                SeaglideSpeedModPluginBz.Log.LogInfo($"Seaglide_Start: ({__instance.name})");
+
+                // Get current modifier
+                float forceModifier = SeaglideSpeedModPluginBz.SeaglideModifier.Value;
+
+                // Add to our list of instances to allow ad-hoc changes to the config settings
+                SeaglideHistoryItem newSeaGlide = new SeaglideHistoryItem(__instance);
+                SeaglideSpeedModPluginBz.SeaglideHistory.Add(newSeaGlide);
+
+                // Update the Seaglide force
+                SeaglideUtils.UpdateSeaglide(__instance, forceModifier);
+            }
+        }
+    }
+}
