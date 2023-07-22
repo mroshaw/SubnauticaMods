43a52596d54f0bfb46651aa0840c71970b1e2e05
mroshaw <oli@mroshaw.co.uk>
2023-07-22 10:58:15 +0100

    Added function to add appropriate child pet class component

--- CreaturePetMod_SN/Utils/PetUtils.cs
+++ CreaturePetMod_SN/Utils/PetUtils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DaftAppleGames.CreaturePetMod_SN.MonoBehaviours.Pets;
+using UnityEngine;
 
 namespace CreaturePetMod_SN.Utils
 {
@@ -7,9 +8,41 @@ namespace CreaturePetMod_SN.Utils
     /// </summary>
     internal static class PetUtils
     {
-        public static void SpawnPet()
+        /// <summary>
+        /// Adds the specified child pet class component to the given creature GameObject
+        /// based on the given PetCreatureType
+        /// </summary>
+        /// <param name="creatureGameObject"></param>
+        /// <param name="petCreatureType"></param>
+        /// <returns></returns>
+        public static Pet AddPetComponent(GameObject creatureGameObject, PetCreatureType petCreatureType, PetName petName)
         {
+            Pet newPet;
 
+            switch (petCreatureType)
+            {
+                case PetCreatureType.CaveCrawler:
+                    newPet = creatureGameObject.AddComponent<CaveCrawlerPet>();
+                    break;
+                case PetCreatureType.BloodCrawler:
+                    newPet = creatureGameObject.AddComponent<BloodCrawlerPet>();
+                    break;
+
+                case PetCreatureType.CrabSquid:
+                    newPet = creatureGameObject.AddComponent<CrabSquidPet>();
+                    break;
+
+                case PetCreatureType.AlienRobot:
+                    newPet = creatureGameObject.AddComponent<AlienRobotPet>();
+                    break;
+                default:
+                    return null;
+            }
+
+            newPet.PetName = petName;
+            newPet.PetCreatureType = petCreatureType;
+
+            return newPet;
         }
     }
 }
