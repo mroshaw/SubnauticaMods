6f39694dc34080ca665edaef6cf8deeeb088badc
mroshaw <oli@mroshaw.co.uk>
2023-09-12 18:29:33 +0100

    Added entries for Pet Fabricator and Pet Console

--- SubnauticaPets/CustomObjects/DatabankEntries.cs
+++ SubnauticaPets/CustomObjects/DatabankEntries.cs
@@ -6,6 +6,7 @@ using DaftAppleGames.SubnauticaPets.MonoBehaviours.Pets.BelowZero;
 #endif
 using DaftAppleGames.SubnauticaPets.Utils;
 using Nautilus.Handlers;
+using Newtonsoft.Json.Linq;
 using static DaftAppleGames.SubnauticaPets.SubnauticaPetsPlugin;
 
 namespace DaftAppleGames.SubnauticaPets.CustomObjects
@@ -16,62 +17,109 @@ namespace DaftAppleGames.SubnauticaPets.CustomObjects
     /// </summary>
     internal static class DatabankEntries
     {
-        private static string PdaPath = "Lifeforms/Fauna";
-        private static string PetDnaKey = "PetDna";
-        
-        // Asset bundle names
-        private static string MainImageTextureName = "PetDnaDataBankMainImageTexture";
-        private static string PopupImageTextureName = "PetDnaDataBankPopupImageTexture";
+        // Pet DNA
+        // Ency keys
+        private static string PetDnaEncyPath = "Lifeforms/Fauna";
+        private static string PetDnaEncyKey = "PetDna";
+        // Asset Bundle refs
+        private static string PetDnaMainImageTexture = "PetDnaDataBankMainImageTexture";
+        private static string PetDnaPopupImageTexture = "PetDnaDataBankPopupImageTexture";
+
+        // Pet Fabricator
+        // Ency keys
+        private static string PetFabricatorEncyKey = "PetFabricator";
+        private static string PetFabricatorEncyPath = "Tech/Habitats";
+        // Asset Bundle refs
+        private static string PetFabricatorMainImageTexture = "PetFabricatorDataBankMainImageTexture";
+        private static string PetFabriactorPopupImageTexture = "PetFabricatorDataBankPopupImageTexture";
+
+        // Pet Console
+        // Ency keys
+        private static string PetConsoleEncyPath = "Tech/Habitats";
+        private static string PetConsoleEncyKey = "PetConsole";
+        // Asset Bundle refs
+        private static string PetConsoleMainImageTexture = "PetConsoleDataBankMainImageTexture";
+        private static string PetConsolePopupImageTexture = "PetConsoleDataBankPopupImageTexture";
+
         /// <summary>
         /// Adds all DataBank entries
         /// </summary>
         public static void ConfigureDataBank()
         {
             Log.LogDebug("DatabankEntries: Setting up Databank...");
-            ConfigureDnaDatabankEntry();
+            
+            // Pet DNA
+            ConfigureDatabankEntry(PetDnaEncyKey, PetDnaEncyPath, PetDnaMainImageTexture, PetDnaPopupImageTexture);
+            SetDnaPickupGoals(PetDnaEncyKey);
+
+            // Pet Fabricator
+            ConfigureDatabankEntry(PetFabricatorEncyKey, PetFabricatorEncyPath, PetFabricatorMainImageTexture, PetFabriactorPopupImageTexture);
+            SetScanGoals(PetFabricatorEncyKey, PetFabricatorFragmentPrefab.PrefabInfo.TechType);
+
+            // Pet Console
+            ConfigureDatabankEntry(PetConsoleEncyKey, PetConsoleEncyPath, PetConsoleMainImageTexture, PetConsolePopupImageTexture);
+            SetScanGoals(PetConsoleEncyKey, PetConsoleFragmentPrefab.PrefabInfo.TechType);
+
             Log.LogDebug("DatabankEntries: Setting up Databank... Done.");
         }
 
         /// <summary>
-        /// Adds the Pet DNA entry to the Databank
+        /// Can be called to "trigger" the Databank entry manually
         /// </summary>
-        private static void ConfigureDnaDatabankEntry()
+        public static void TriggerDataBankEntry(string encyKey)
         {
-            Log.LogDebug("DatabankEntries: Setting up PetDNA entry...");
-            PDAHandler.AddEncyclopediaEntry(PetDnaKey, PdaPath, null, null,
-                ModUtils.GetTexture2DFromAssetBundle(MainImageTextureName),
-                ModUtils.GetSpriteFromAssetBundle(PopupImageTextureName));
-            Log.LogDebug("DatabankEntries: Setting up ItemGoals...");
-#if SUBNAUTICA
-            StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, AlienRobotPet.DnaBuildablePrefabInfo.TechType);
-            StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, BloodCrawlerPet.DnaBuildablePrefabInfo.TechType);
-            StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, CaveCrawlerPet.DnaBuildablePrefabInfo.TechType);
-            StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, CrabSquidPet.DnaBuildablePrefabInfo.TechType);
-#endif
 #if SUBNAUTICAZERO
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, PenglingAdultPet.DnaBuildablePrefabInfo.TechType);
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, PenglingBabyPet.DnaBuildablePrefabInfo.TechType);
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, PinnicaridPet.DnaBuildablePrefabInfo.TechType);
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, SnowStalkerBabyPet.DnaBuildablePrefabInfo.TechType);
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, TrivalveYellowPet.DnaBuildablePrefabInfo.TechType);
-            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(PetDnaKey, Story.GoalType.Encyclopedia, TrivalveBluePet.DnaBuildablePrefabInfo.TechType);
+            PDAEncyclopedia.Add(encyKey, true, true);
 #endif
-            Log.LogDebug("DatabankEntries: Setting up ItemGoals... Done.");
+
+#if SUBNAUTICA
+            PDAEncyclopedia.Add(encyKey, true);
+#endif
+        }
+
+        private static void ConfigureDatabankEntry(string encyKey, string encyPath, string mainImageTextureName,
+            string popupImageTextureName)
+        {
+            Log.LogDebug($"DatabankEntries: Setting up {encyKey} entry...");
+            PDAHandler.AddEncyclopediaEntry(encyKey, encyPath, null, null,
+                ModUtils.GetTexture2DFromAssetBundle(mainImageTextureName),
+                ModUtils.GetSpriteFromAssetBundle(popupImageTextureName));
+
             Log.LogDebug("DatabankEntries: Setting up PetDNA entry... Done.");
         }
 
         /// <summary>
-        /// Called to "trigger" the Databank entry
+        /// Sets up goals based on collection of DNA samples
         /// </summary>
-        public static void AddPetDnaDataBankEntry()
+        /// <param name="encyKey"></param>
+        private static void SetDnaPickupGoals(string encyKey)
         {
-#if SUBNAUTICAZERO
-            PDAEncyclopedia.Add(PetDnaKey, true, true);
-#endif
-
+            Log.LogDebug("DatabankEntries: Setting up ItemGoals...");
 #if SUBNAUTICA
-            PDAEncyclopedia.Add(PetDnaKey, true);
+            StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, AlienRobotPet.DnaBuildablePrefabInfo.TechType);
+            StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, BloodCrawlerPet.DnaBuildablePrefabInfo.TechType);
+            StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, CaveCrawlerPet.DnaBuildablePrefabInfo.TechType);
+            StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, CrabSquidPet.DnaBuildablePrefabInfo.TechType);
 #endif
+#if SUBNAUTICAZERO
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, PenglingAdultPet.DnaBuildablePrefabInfo.TechType);
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, PenglingBabyPet.DnaBuildablePrefabInfo.TechType);
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, PinnicaridPet.DnaBuildablePrefabInfo.TechType);
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, SnowStalkerBabyPet.DnaBuildablePrefabInfo.TechType);
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, TrivalveYellowPet.DnaBuildablePrefabInfo.TechType);
+            Nautilus.Handlers.StoryGoalHandler.RegisterItemGoal(encyKey, Story.GoalType.Encyclopedia, TrivalveBluePet.DnaBuildablePrefabInfo.TechType);
+#endif
+            Log.LogDebug("DatabankEntries: Setting up ItemGoals... Done.");
+        }
+
+        /// <summary>
+        /// Sets up goals based on scanning of fragment TechTypes
+        /// </summary>
+        /// <param name="encyKey"></param>
+        /// <param name="scanTechType"></param>
+        private static void SetScanGoals(string encyKey, TechType scanTechType)
+        {
+            PDAHandler.AddCustomScannerEntry(scanTechType, 2.0f, false, encyKey);
         }
     }
 }
