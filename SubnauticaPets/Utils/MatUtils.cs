0e6b3bffcd4ea81d480a85c6d1aa7a7fff257feb
mroshaw <oli@mroshaw.co.uk>
2023-09-15 17:16:37 +0100

    Added util to swap textures on a game object

---
+++ SubnauticaPets/Utils/MatUtils.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DaftAppleGames.SubnauticaPets.Utils
+{
+    /// <summary>
+    /// Static utilities class for Material methods
+    /// </summary>
+    internal static class MatUtils
+    {
+        public static void SetMaterialTexture(GameObject go, string oldTextureName, string bundleTextureName)
+        {
+            Renderer[] renderers = go.GetComponentsInChildren<Renderer>(true);
+            Texture texture = ModUtils.GetTexture2DFromAssetBundle(bundleTextureName);
+
+            foreach (Renderer renderer in renderers)
+            {
+                if (renderer.material.mainTexture.name == oldTextureName)
+                {
+                    renderer.material.mainTexture = texture;
+                }
+            }
+        }
+    }
+}
