a15a33bf02095290229070f5ee4cb8b9c876a233
mroshaw <oli@mroshaw.co.uk>
2023-09-15 17:18:48 +0100

    Added spawn util methods to align and settle spawns

---
+++ SubnauticaPets/MonoBehaviours/Utils/AlignToFloorOnStart.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using static DaftAppleGames.SubnauticaPets.SubnauticaPetsPlugin;
+
+namespace DaftAppleGames.SubnauticaPets.MonoBehaviours.Utils
+{
+    /// <summary>
+    /// Simple MonoBehaviour to align object transform to the floor on start
+    /// </summary>
+    internal class AlignToFloorOnStart : MonoBehaviour
+    {
+        public Vector3 HitOffset = new Vector3(0.0f, 0.0f, 0.0f);
+
+        /// <summary>
+        /// Align with the floor
+        /// </summary>
+        public void Start()
+        {
+            transform.position = GetFloorPosition();
+        }
+
+        /// <summary>
+        /// Get offset position of floor beneath object
+        /// </summary>
+        /// <returns></returns>
+        private Vector3 GetFloorPosition()
+        {
+            RaycastHit hit;
+            bool isHit = Physics.Raycast(transform.position, Vector3.down, out hit, 5.0f);
+            if (isHit)
+            {
+                Log.LogDebug($"AlignToFloorOnStart: Aligning object {gameObject.name} to floor.");
+                return hit.point + HitOffset;
+            }
+            else
+            {
+                Log.LogDebug($"AlignToFloorOnStart: Raycast failed to hit while aligning object {gameObject.name} to floor.");
+                return transform.position;
+            }
+        }
+
+    }
+}
---
+++ SubnauticaPets/MonoBehaviours/Utils/FreezeOnSettle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DaftAppleGames.SubnauticaPets.MonoBehaviours.Utils
+{
+
+    /// <summary>
+    /// Locks RigidBody constraints once the object has settled
+    /// </summary>
+    internal class FreezeOnSettle : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        private bool _isFrozen = true;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            StartCoroutine(WaitToStartAsync());
+        }
+
+        /// <summary>
+        /// Unity Awake method. Runs every frame so remove this if not required.
+        /// Runs frequently, so remove if not required.
+        /// </summary>
+        public void Update()
+        {
+            if (_isFrozen)
+            {
+                return;
+            }
+
+            // While we're moving, do nothing
+            if (_rigidbody.velocity.magnitude > 0.001f || !IsOnFloor())
+            {
+                return;
+            }
+
+            // Settled, freeze constraints
+            _rigidbody.isKinematic = true;
+            _isFrozen = true;
+        }
+
+        /// <summary>
+        /// Wait a few seconds before starting
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator WaitToStartAsync()
+        {
+            yield return new WaitForSeconds(2.0f);
+            _rigidbody.isKinematic = false;
+            _rigidbody.useGravity = true;
+            _isFrozen = false;
+        }
+
+        /// <summary>
+        /// Determines if object is on the floor
+        /// </summary>
+        /// <returns></returns>
+        private bool IsOnFloor()
+        {
+            RaycastHit hit;
+            return Physics.Raycast(transform.position, Vector3.down, out hit, 0.2f);
+        }
+    }
+}
